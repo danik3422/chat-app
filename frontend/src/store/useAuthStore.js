aca827161fb155fcd38ae8e09d24bb2395969906
Danylo Syloats <mr.siloacd@gmail.com>
2025-05-22 05:07:43 +0200

    Added login , signup, profile pages. Added api routes for auth and message

---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,92 @@
+import toast from 'react-hot-toast'
+import { create } from 'zustand'
+import { axiosInstance } from '../lib/axios'
+
+export const useAuthStore = create((set) => ({
+	authUser: null,
+	isSigningUp: false,
+	isLoggingIn: false,
+	isUpdatingProfile: false,
+
+	isCheckingAuth: true,
+
+	checkAuth: async () => {
+		try {
+			const res = await axiosInstance.get('/auth/get-user')
+			set({
+				authUser: res.data,
+			})
+		} catch (error) {
+			console.log('Error checking auth', error)
+			set({
+				authUser: null,
+			})
+		} finally {
+			set({
+				isCheckingAuth: false,
+			})
+		}
+	},
+
+	signup: async (data) => {
+		set({ isSigningUp: true })
+		try {
+			const res = await axiosInstance.post('/auth/signup', data)
+			toast.success('Account created successfully')
+			set({
+				authUser: res.data,
+			})
+		} catch (error) {
+			toast.error(error.response.data.message)
+		} finally {
+			set({
+				isSigningUp: false,
+			})
+		}
+	},
+
+	logout: async () => {
+		try {
+			await axiosInstance.post('/auth/logout')
+			set({
+				authUser: null,
+			})
+			toast.success('Logged out successfully')
+		} catch (error) {
+			toast.error(error.response.data.message)
+		}
+	},
+
+	login: async (data) => {
+		set({ isLoggingIn: true })
+
+		try {
+			const res = await axiosInstance.post('/auth/login', data)
+
+			toast.success('Account logged in successfully')
+			set({
+				authUser: res.data,
+			})
+		} catch (error) {
+			toast.error(error.response.data.message)
+		} finally {
+			set({ isLoggingIn: false })
+		}
+	},
+
+	updateProfile: async (data) => {
+		set({ isUpdatingProfile: true })
+
+		try {
+			const res = await axiosInstance.put('/auth/update-profile', data)
+			set({
+				authUser: res.data,
+			})
+			toast.success('Profile updated successfully')
+		} catch (error) {
+			toast.error(error.response.data.message)
+		} finally {
+			set({ isUpdatingProfile: false })
+		}
+	},
+}))
