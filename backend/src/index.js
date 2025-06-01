1cb8f10e9bc163b8b38e3877c170b50ca1cb2011
Danylo Syloats <mr.siloacd@gmail.com>
2025-06-01 12:30:09 +0200

    feat: reintroduce production static file serving in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,6 +23,14 @@ app.use(
 app.use('/api/auth', authRoutes)
 app.use('/api/message', messageRoutes)
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, 'frontend/dist')))
+
+	app.get('/{*any}', (req, res) => {
+		res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'))
+	})
+}
+
 server.listen(PORT, () => {
 	console.log('Server is running on port ' + PORT)
 	connectDB()
